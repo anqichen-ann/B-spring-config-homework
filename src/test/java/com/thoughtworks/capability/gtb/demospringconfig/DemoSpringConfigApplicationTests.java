36dd484408a7a2c1f4e622d87d84b5482d4f2baa
annie <anqi.chen@thoughtworks.com>
2020-10-26 21:25:36 +0800

    feat p2s1

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
@@ -1,13 +1,23 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
-class DemoSpringConfigApplicationTests {
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = {"spring.config.location=classpath:application-test.properties"})
+
+public class DemoSpringConfigApplicationTests {
+	@Autowired
+	private LevelController levelController;
 
 	@Test
 	void contextLoads() {
+		String levelStr = levelController.getLevel();
+		assert(levelStr).equals("advanced");
+
 	}
 
 }
